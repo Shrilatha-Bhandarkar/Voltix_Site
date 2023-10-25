450256e51fc9b486dd2aae82f573cc4ca0ddcd34
Shrilatha-Bhandarkar <shribhandarkar26@gmail.com>
2023-10-25 12:10:30 +0530

    changes

---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VoltixSolutionHeader: React.FC = () => {
+  return (
+    <header>
+      <h1>VOLTIX SOLUTION</h1>
+      <p>
+        We are not afraid to say no to projects that we cannot do well. We would
+        rather focus on a few projects and do them right than take on too much
+        and sacrifice quality.
+      </p>
+      <nav>
+        <a href="#">Company</a>
+        <a href="#">About Us</a>
+        <a href="#">Get in touch</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Support</a>
+        <a href="#">Information</a>
+      </nav>
+    </header>
+  );
+};
+
+export default VoltixSolutionHeader;
