0928057bbff708aced7a2d22b8fef71a441aa46f
Shahistha <shahisthanavab@gmail.com>
2023-10-25 19:11:40 +0530

    changes

--- frontend/src/Pages/Services.tsx
+++ frontend/src/Pages/Services.tsx
@@ -6,26 +6,32 @@ function Services() {
     {
       title: 'Web Development',
       description: 'Custom web solutions tailored to your needs.',
+      content: 'Web development content goes here.',
     },
     {
       title: 'Mobile App Development',
       description: 'We build mobile apps for both iOS and Android platforms.',
+      content: 'Mobile app development content goes here.',
     },
     {
       title: 'UI/UX Design',
       description: 'Creating user-friendly and visually appealing interfaces.',
+      content: 'UI/UX design content goes here.',
     },
     {
       title: 'Digital Marketing',
       description: 'Effective digital marketing strategies to boost your online presence.',
+      content: 'Digital marketing content goes here.',
     },
     {
       title: 'SEO Services',
-      description: 'Improving your websites visibility on search engines.',
+      description: 'Improving your website\'s visibility on search engines.',
+      content: 'SEO services content goes here.',
     },
     {
       title: 'Content Creation',
       description: 'Engaging and informative content to attract your audience.',
+      content: 'Content creation content goes here.',
     },
   ];
 
@@ -40,8 +46,28 @@ function Services() {
           <div key={index} className="service-card">
             <h2>{service.title}</h2>
             <p>{service.description}</p>
+            <h1>Service Details</h1>
+            <p>{service.content}</p>
           </div>
         ))}
+      </div>
+      <div className="additional-contents">
+        <div className="additional-content">
+          <h1>Additional Content 1</h1>
+          <p>Additional content paragraph 1 goes here.</p>
+        </div>
+        <div className="additional-content">
+          <h1>Additional Content 2</h1>
+          <p>Additional content paragraph 2 goes here.</p>
+        </div>
+        <div className="additional-content">
+          <h1>Additional Content 3</h1>
+          <p>Additional content paragraph 3 goes here.</p>
+        </div>
+        <div className="additional-content">
+          <h1>Additional Content 4</h1>
+          <p>Additional content paragraph 4 goes here.</p>
+        </div>
       </div>
     </div>
   );
