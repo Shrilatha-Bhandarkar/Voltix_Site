663190e964e507965abe44735ef5b16e51992885
Shahistha <shahisthanavab@gmail.com>
2023-10-25 12:05:22 +0530

    changes

--- frontend/src/Pages/About.tsx
+++ frontend/src/Pages/About.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './About.css';
 function About() {
   return (
     <div className="page">
      
-        <h1>About Us</h1>
+        <h1>We are Voltix Solution</h1>
       
       <p>
       A Calicut-based company called Voltix Solution creates websites and mobile applications. We're constantly on the lookout for amazing customers with unique ideas. Because they enable us to advance as a business, we find these difficulties and improvements to be worthwhile. We are one of Calicut's top online and mobile app developers because of our workplace culture, which is based on continuous improvement. Along with applications and designs, we provide more. To satisfy the needs of our customers, we provide a well designed solution.
@@ -11,14 +12,14 @@ By working with you to comprehend your demands and create a unique solution that
 Voltix Solution is the business for you if you're looking for a web or mobile app development firm that can assist you in reaching your objectives. Our commitment to you includes giving you the greatest service and outcomes is unwavering.
       </p>
       <img src="about-image.jpg" alt="About Us Image" />
-      <p>
-      How we work
+      <p className="how-we-work">
+     <h1 className='howwework'> How we work</h1>
 Voltix Solution is always prepared for the most spectacular clientele from various professions to contact us. the people who have extraordinary ideas to realize. If you look at our client list, you will see that we have always been successful in acquiring some.
 They both test us and help us get better, in our opinion. We became who we are today by working on projects that maximize our potential. The continual progress we have made thanks to them has shaped our workplace culture.
 As a result, we are known as one of Calicut's top web and mobile app developers. You can count on us to deliver more than simply an application or a design. What we provide is a thoughtfully designed solution that accomplishes its goal and produces the result that our clients are looking for.
       </p>
       <footer>
-        <p>Contact us for more details: contact@example.com</p>
+        
       </footer>
     </div>
   );
