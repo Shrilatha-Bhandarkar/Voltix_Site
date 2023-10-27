4d932446904499e321c7a5846281f44116acb008
shravya kotiyan <shravya1573@gmail.com>
2023-10-27 11:58:42 +0530

    changes

--- backend/models/servicesModel.ts
+++ backend/models/servicesModel.ts
@@ -4,6 +4,7 @@ interface IService extends Document {
   service_title: string;
   service_description: string;
   service_category: string;
+  service_template:string;
   image_url: string;
   created_at: Date;
   created_by: string;
@@ -13,6 +14,7 @@ const servicesSchema = new mongoose.Schema<IService>({
   service_title: { type: String, required: true, unique: true, trim: true },
   service_description: { type: String, required: true },
   service_category: { type: String, required: true },
+  service_template: { type: String, required: true },
   image_url: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   created_by: { type: String, required: true}
