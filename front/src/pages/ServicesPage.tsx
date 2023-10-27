b8f39159d01726f831ba98fda7d39064f255258b
Shrilatha-Bhandarkar <shribhandarkar26@gmail.com>
2023-10-27 09:36:46 +0530

    changes

--- front/src/pages/ServicesPage.tsx
+++ front/src/pages/ServicesPage.tsx
@@ -4,7 +4,6 @@ import Services from '../components/services'
 const ServicesPage : React.FC =()=>{
     return(
         <div>
-            <h1>Services</h1>
             <Services />
         </div>
     );
