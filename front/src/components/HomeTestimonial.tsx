68e8d5a33f68a53fba7288072464f8f5d82fcbfc
Shrilatha-Bhandarkar <shribhandarkar26@gmail.com>
2023-10-30 09:45:03 +0530

    changes

--- front/src/components/HomeTestimonial.tsx
+++ front/src/components/HomeTestimonial.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { fetchTestimonials } from "../services/api";
 import './styles/Home.css';
 import StarRating from './StarRating'; // Import the StarRating component
+import Slider from "react-slick"; // Import the Slider component from react-slick
+import "slick-carousel/slick/slick.css"; // Import slick-carousel CSS
+import "slick-carousel/slick/slick-theme.css"; // Import slick-carousel theme CSS
 
 interface ITestimonial {
   _id: string;
@@ -23,38 +26,39 @@ const HomeTestimonial = () => {
       });
   }, []);
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Number of cards to show at once
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="container">
       <h1 className="head">Trusted by Millions of Customers.</h1>
       <h5 className="sub-head">
         We have been successful in developing business solutions in large part because of our talented and devoted workforce.
       </h5>
-      <div className="row">
+      <Slider {...sliderSettings}> {/* Use the Slider component with settings */}
         {testimonial.map((entry) => (
-          <div className="col-md-4" key={entry._id}>
-            <div className="card mb-4 mt-4">
-              <div className="card-body d-flex flex-column">
-              {/* <h5 className="card-title text-center with-underline">
-               {entry.testimonial_author}
-              </h5> */}
-              <div className="card-title-container text-center">
-               <h5 className="card-title">
-                 {entry.testimonial_author}
-               </h5>
-              <div className="line"></div>
-             </div>
-
-                <h6 className="card-text text-center">{entry.testimonial_content}</h6>
-                <div className="star-rating text-center">
-                  <StarRating rating={entry.rating} maxRating={5} />
-                </div>
-
-                <div className="mt-auto text-end"></div>
+          <div className="card1" key={entry._id}>
+            <div className="card1-body d-flex flex-column">
+              <div className="card1-title-container text-center">
+                <h5 className="card1-title">
+                  {entry.testimonial_author}
+                </h5>
+                <div className="line"></div>
               </div>
+              <h6 className="card1-text text-center">{entry.testimonial_content}</h6>
+              <div className="star-rating text-center">
+                <StarRating rating={entry.rating} maxRating={5} />
+              </div>
+              <div className="mt-auto text-end"></div>
             </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
