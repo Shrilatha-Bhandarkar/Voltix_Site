e5c1e7f4dfaaa025a422135706c462a210ba8527
shravya kotiyan <shravya1573@gmail.com>
2023-11-04 08:35:03 +0530

    push

--- front/src/components/HomeHead.tsx
+++ front/src/components/HomeHead.tsx
@@ -5,7 +5,7 @@ const HomeHead = () => {
       <div className="row">
       <img
         className="img-fluid hh-img"
-        src="https://s3.ap-southeast-1.wasabisys.com/voltixsite/1698732684989_Home%20page.png?AWSAccessKeyId=U1CFVX3BJHI8U6L3C3CH&Expires=1699063671&Signature=dX5XGS%2BWeY8zpYEmJf76X0EZnmo%3D"
+        src="https://s3.ap-southeast-1.wasabisys.com/voltixsite/1698732684989_Home%20page.png?AWSAccessKeyId=S86YIULFDY6GZJCCOCWB&Expires=1699109866&Signature=2vOqcDlg73ZSn9iC2dWJeA6yNCI%3D"
         alt="Welcome!"
       ></img><div className="col-1">
       <div className="textStyleLarge1">
