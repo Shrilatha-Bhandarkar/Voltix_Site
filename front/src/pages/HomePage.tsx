9ed934ea02e6245cefd6ef0a9ab88b6d798f0c23
11vib <vibhahb9140@gmail.com>
2023-11-06 11:58:24 +0530

    changes

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import HomeHead from "../components/HomeHead";
 import Preloader from "../components/Preloader";
 import React, { useState, useEffect } from "react";
 import "./styles/Page.css";
+import "../components/styles/Contact.css";
 
 const HomePage: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -28,35 +29,16 @@ const HomePage: React.FC = () => {
       {isLoading ? (
         <Preloader />
       ) : (
-        //   <div className="container-fluid">
-        //       <div className="area">
-        //           <div className="services">
-        //               <HomeHead />
-        //               <HomeService />
-        //               <HomeProject />
-        //               <HomeTestimonial />
-        //           </div>
-        //       </div>
-        //       <div className="circles">
-        //           <ul>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //               <li></li>
-        //           </ul>
-        //       </div>
-        //   </div>
+        <div className="container-fluid cbg">
+        <div className="bg"></div>
+        <div className="bg bg2"></div>
+        <div className="bg bg3"></div>
         <div className="services">
           <HomeHead />
           <HomeService />
           <HomeProject />
           <HomeTestimonial />
+        </div>
         </div>
       )}
     </div>
