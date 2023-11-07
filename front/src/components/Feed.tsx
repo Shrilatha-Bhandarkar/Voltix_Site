018209ce80c1d88bb1ec91cc636ea3d9a8a76040
Shrilatha-Bhandarkar <shribhandarkar26@gmail.com>
2023-11-07 11:13:36 +0530

    changes

--- front/src/components/Feed.tsx
+++ front/src/components/Feed.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './styles/Feed.css';
-import './styles/Contact.css';
+
 const Feed: React.FC = () => {
   return (
     <div className='text-center'>
       <div className="bg"></div>
-        <div className="bg bg2"></div>
-        <div className="bg bg3"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
       <iframe
         title="Facebook Page Plugin"
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61550596620253&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="500"
-        height="600"
         style={{ border: 'none', overflow: 'hidden'}}
         allowFullScreen
         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
--- front/src/components/HomeHead.tsx
+++ front/src/components/HomeHead.tsx
@@ -7,7 +7,8 @@ const HomeHead = () => {
         className="img-fluid hh-img"
         src="https://s3.ap-southeast-1.wasabisys.com/voltixsite/1698732684989_Home%20page.png?AWSAccessKeyId=UVDJVBANJ7BCSIKWMO81&Expires=1699303494&Signature=erMoMWQKa60oz0JFKjCo2%2BZX%2BE0%3D"
         alt="Welcome!"
-      ></img><div className="col-1">
+      ></img>
+      <div className="col-1">
       <div className="textStyleLarge1">
         <p>
           Bring your <br></br>ideas to life!
